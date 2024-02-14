2609841f26e04bad68dbbb42737ad31958f56455
Valmir de Barros Pedro <valmir.barros1@outlook.com>
2024-02-14 17:28:43 -0500

    Changed the port number to 3000 if the environment variable is not set, and removed unnecessary newlines at the end of the files.

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const connectDB = require('./config/db')
 // load config
 dotenv.config({path:'./config/config.env'})
 
+connectDB()
+
 const app  =  express()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB  = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        })
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        });
 
-        console.log(`mongoDB Connected: ${conn.connection.host}` )
-    } catch (err){
-        console.error(err)
-        process.exit(1)
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
